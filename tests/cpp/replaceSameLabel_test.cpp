898aba8d8206ab441a4643b7c31686f2cd68ea95
ozanarmagan <o.armagan2020@gtu.edu.tr>
2024-02-05 13:56:44 +0300

    Add logs for test

--- tests/cpp/replaceSameLabel_test.cpp
+++ tests/cpp/replaceSameLabel_test.cpp
@@ -16,48 +16,60 @@ int main() {
 
     hnswlib::InnerProductSpace space(d);
 
+    std::cout << "Generating random data" << std::endl;
+    std::cout << "Initial batch size: " << max_elements << std::endl;
     float* initial_batch = new float[d * max_elements];
     for (int i = 0; i < d * max_elements; i++) {
         initial_batch[i] = distrib_real(rng);
     }
+
+    std::cout << "Update batch size: " << num_elements << std::endl;
     float* update_batch = new float[d * num_elements];
     for (int i = 0; i < d * num_elements; i++) {
         update_batch[i] = distrib_real(rng);
     }
 
+    std::cout << "Building index" << std::endl;
     hnswlib::HierarchicalNSW<float>* alg_hnsw = new hnswlib::HierarchicalNSW<float>(&space, 1024, 16, 200, 456, true);
 
     std::vector<int> labels;
 
+    std::cout << "Adding initial batch" << std::endl;
     for(int row; row < max_elements; row++) {
         int label = distrib_int(rng);
         labels.push_back(label);
         alg_hnsw->addPoint((void*)(initial_batch + d * row), label, true);
     };
 
+    std::cout << "Deleting half of the initial batch" << std::endl;
     for (int i = 0; i < labels.size() / 2; i++) {
         if(!alg_hnsw->isMarkedDeleted(alg_hnsw->label_lookup_[labels[i]]))
             alg_hnsw->markDelete(labels[i]);
     }
-
+    
+    std::cout << "Updating index size" << std::endl;
     size_t curr_ele_count = alg_hnsw->getCurrentElementCount();
     if(curr_ele_count + max_elements > alg_hnsw->getMaxElements()) {
             alg_hnsw->resizeIndex((curr_ele_count + max_elements) * 1.3);
     }
 
+    std::cout << "Adding update batch" << std::endl;
     for(int row; row < num_elements; row++) {
         alg_hnsw->addPoint((void*)(update_batch + d * row), 42, true);
     };
 
 
+    std::cout << "Searching for 10 nearest neighbors" << std::endl;
     auto results = alg_hnsw->searchKnnCloserFirst((void*)(initial_batch), 10);
     
     // check if the search results contain duplicate labels
+    std::cout << "Checking search results for duplicate labels" << std::endl;
     std::unordered_set<int> labels_set;
     for (int i = 0; i < 10; i++) {
         labels_set.insert(results[i].second);
     }
     if (labels_set.size() != 10) {
+        std::cout << "Search results contain duplicate labels" << std::endl;
         throw std::runtime_error("Search results contain duplicate labels");
     }
 
