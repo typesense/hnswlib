6503d1ce4bbf55f0191b51ecf6b799e39db5b8f7
ozanarmagan <o.armagan2020@gtu.edu.tr>
2024-02-05 12:23:20 +0300

    Refactor test

--- tests/cpp/replaceSameLabel_test.cpp
+++ tests/cpp/replaceSameLabel_test.cpp
@@ -20,18 +20,11 @@ int main() {
     for (int i = 0; i < d * max_elements; i++) {
         initial_batch[i] = distrib_real(rng);
     }
-    float* update_bactch = new float[d * num_elements];
+    float* update_batch = new float[d * num_elements];
     for (int i = 0; i < d * num_elements; i++) {
-        update_bactch[i] = distrib_real(rng);
+        update_batch[i] = distrib_real(rng);
     }
 
-    std::vector<int> rand_labels(max_elements);
-    for (int i = 0; i < max_elements; i++) {
-        rand_labels[i] = i;
-    }
-    std::shuffle(rand_labels.begin(), rand_labels.end(), rng);
-
-    int iter = 0;
     hnswlib::HierarchicalNSW<float>* alg_hnsw = new hnswlib::HierarchicalNSW<float>(&space, 1024, 16, 200, 456, true);
 
     std::vector<int> labels;
@@ -53,12 +46,12 @@ int main() {
     }
 
     for(int row; row < num_elements; row++) {
-        int label = rand_labels[row] + max_elements;
-        alg_hnsw->addPoint((void*)(update_bactch + d * row), 42, true);
+        alg_hnsw->addPoint((void*)(update_batch + d * row), 42, true);
     };
 
 
     auto results = alg_hnsw->searchKnnCloserFirst((void*)(initial_batch), 10);
+    
     // check if the search results contain duplicate labels
     std::unordered_set<int> labels_set;
     for (int i = 0; i < 10; i++) {
@@ -69,7 +62,7 @@ int main() {
     }
 
     delete[] initial_batch;
-    delete[] update_bactch;
+    delete[] update_batch;
     delete alg_hnsw;
     return 0;
 }
